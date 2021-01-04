d458bda1031c15f782245cdcdac3311dc954a972
rhoitjadhav <jadhavrohit497@gmail.com>
2021-01-04 10:57:35 +0530

    cookie_consent column added

--- src/models/users.py
+++ src/models/users.py
@@ -1,5 +1,6 @@
 from src.models import db
 
+
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20))
@@ -7,3 +8,4 @@ class Users(db.Model):
     email = db.Column(db.String(20))
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20))
+    cookie_consent = db.Column(db.String(10))
