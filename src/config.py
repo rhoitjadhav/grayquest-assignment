f0576223c40785860d97d31f02338c0e646ed537
rhoitjadhav <jadhavrohit497@gmail.com>
2021-01-04 11:27:40 +0530

    add SEND_FILE_MAX_AGE_DEFAULT for caching files

--- src/config.py
+++ src/config.py
@@ -1,8 +1,9 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname((__file__))))
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 SECRET_KEY = 'dev_secret_key'
 ENV = 'development'
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SEND_FILE_MAX_AGE_DEFAULT = 0
