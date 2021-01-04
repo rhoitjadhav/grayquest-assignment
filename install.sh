5e54b1b0e3742b660f17029eb1a3fd1d57acfc15
rhoitjadhav <jadhavrohit497@gmail.com>
2021-01-04 12:41:28 +0530

    installation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit if any command fails
+set -e
+
+# Install Python3
+sudo apt install python3
+
+# Install pip
+sudo apt install python3-pip
+
+# Install pipenv
+pip3 install pipenv
+
+# Install python packages
+pipenv install
+
+# Install sqlite3 package
+sudo apt install sqlite3
